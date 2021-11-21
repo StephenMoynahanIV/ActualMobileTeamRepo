3689f34a03f7c0a1ddc688ea70b3c05f4ba5ec61
StephenMoynahanIV <78392892+StephenMoynahanIV@users.noreply.github.com>
2021-11-21 17:35:53 -0500

    Reverted small change

--- src/components/UMass.jsx
+++ src/components/UMass.jsx
@@ -7,7 +7,7 @@ function UMass() {
         <div class="row align-items-center my-5">
           <div class="col-lg-7">
             <img
-              class="img-fluid rounded mb-4 bm-lg-0"
+              class="img-fluid rounded mb-4 mb-lg-0"
               // src="http://placehold.it/900x400"
               src="https://cdn.discordapp.com/attachments/910276735038267422/912099519980781608/1200px-University_of_Massachusetts_Amherst_seal_Small.jpeg"
               alt=""
