7bcecaac9b311bb45ceb4b39e4d73fc3aa013411
yonsiojnag <yonsiojnag@gmail.com>
2021-11-21 17:07:39 -0500

    Create placeList.js

---
+++ src/data/placeList.js
@@ -0,0 +1,11 @@
+const peopleList = [
+    {
+        Name: "Mich Trimms",
+        Email: "michtrimms@umass.edu",
+        Phone: "413-545-4150",
+        Department: "College of Info & Computer Sciences",
+        Address: "Computer Science Research Ctr, Rm 212",
+        Photo: ,
+    },
+
+export 
