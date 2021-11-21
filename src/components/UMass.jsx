46ecb659fc4537d0cfcd442496b91f5dbd8c331e
StephenMoynahanIV <78392892+StephenMoynahanIV@users.noreply.github.com>
2021-11-21 17:30:53 -0500

    Instruction text for UMass tab

--- src/components/UMass.jsx
+++ src/components/UMass.jsx
@@ -7,19 +7,19 @@ function UMass() {
         <div class="row align-items-center my-5">
           <div class="col-lg-7">
             <img
-              class="img-fluid rounded mb-4 mb-lg-0"
+              class="img-fluid rounded mb-4 bm-lg-0"
               // src="http://placehold.it/900x400"
               src="https://cdn.discordapp.com/attachments/910276735038267422/912099519980781608/1200px-University_of_Massachusetts_Amherst_seal_Small.jpeg"
               alt=""
             />
           </div>
           <div class="col-lg-5">
-            <h1 class="font-weight-light">UMass</h1>
+            <h1 class="font-weight-light">UMass People Finder Instructions</h1>
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
+              Welcome to UMass People Finder! You can use the tabs above to <br />
+              -find Places in or around UMass <br />
+              -find People in or related to UMass, <br />
+              -navigate through your saved favorite information
             </p>
           </div>
         </div>
