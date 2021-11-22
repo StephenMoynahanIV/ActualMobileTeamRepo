46954d91306347f8f9a40478d10614fbdc7f2866
Yuni <syjeong215@gmail.com>
2021-11-22 01:42:29 -0500

    minor changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="footer" style={{marginTop: "130px"}}>
+    <div className="footer" style={{marginTop: "70px"}}>
       <footer class="py-3 bg-danger fixed-bottom">
         <div class="container">
           <p class="m-0 text-center text-white">
