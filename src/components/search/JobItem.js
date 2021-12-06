ba0d436d2cc28ec3523168961d79281f709844f6
Yuni <syjeong215@gmail.com>
2021-12-06 00:43:32 -0500

    remove from Favorites added. Checks for duplicate items when adding to Fav. Button styling changed.

--- src/components/search/JobItem.js
+++ src/components/search/JobItem.js
@@ -15,35 +15,46 @@ function JobItem({listing}) {
 
         const insertAsync = async() => {
             // await db("FAVORITES").insert(easybaseData[1]).one();
-            await db("FAVORITES").insert({
-            title: title,
-            name: name,
-            tag1: tag1,
-            tag2: tag2,
-            description: description,
-            locationText: location,
-            photo: photo,
-            rating: rating
-            }).one();
+            var all = await db('FAVORITES').return().where({title: title}).all()
+
+            if (all.length == 0) {
+                await db("FAVORITES").insert({
+                            title: title,
+                            name: name,
+                            tag1: tag1,
+                            tag2: tag2,
+                            description: description,
+                            locationText: location,
+                            photo: photo,
+                            rating: rating
+                            }).one();
+            }
           }
+
         insertAsync();
-    
+    }
+
+    function deleteFromFavorites(title) {
+        const deleteAsync = async() => {
+            await db("FAVORITES").delete().where({title: title}).all()
+        }
+        deleteAsync();
     }
 
     return (
         <div className="job-item-container">
-            <div class="Row">
+            <div className="Row">
                 <h2>
                     {listing.title}
                     <h5 style={{color: "maroon"}}>| {listing.name}</h5>
                 </h2>
             </div>
 
-            <div class="Row">
-                <div class="place-image">
+            <div className="Row">
+                <div className="place-image">
                     <img src={listing.photo} />
                 </div>
-                <div clss="Column">
+                <div className="Column">
                     <p>{listing.description}</p>
                     <div className="job-tags">
                         <a>{listing.tag1}</a>
@@ -53,11 +64,12 @@ function JobItem({listing}) {
                     <div>
                         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
                         {StarRatings(parseFloat(listing.rating))}
-                        <button className='action-button' type="button" onClick={insertToFavorites.bind(this, 
+                        <button className='fav-button' type="button" onClick={insertToFavorites.bind(this, 
                             listing.locationtext, listing.tag1, listing.tag2, listing.name, listing.title,
                             listing.photo, listing.description, listing.rating)}> Add To Favorites </button>
-
-                        <button className='action-button' type="button" onClick={open.bind(this, listing.locationtext)}> Get Directions </button>
+                        <button className='fav-button' type="button" onClick={deleteFromFavorites.bind(this, listing.title)}>Remove from Favorites</button>
+                        <br />
+                        <button className='map-button' type="button" onClick={open.bind(this, listing.locationtext)}> Get Directions </button>
                     </div>
                 </div>
                 <br />
