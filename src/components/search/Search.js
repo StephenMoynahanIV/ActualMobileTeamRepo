02bbe8ff9ebb116e08108b21345a431b6f10edd3
StephenMoynahanIV <78392892+StephenMoynahanIV@users.noreply.github.com>
2021-12-07 10:17:55 -0500

    removed all component stuff

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -53,13 +53,6 @@ function Search({ details }) {
                     placeholder="Search by Name, Number, Type, Department..."
                     onChange={handleChange}
                 />
-                <form>
-                    <select className="list-sort">
-                        <option value="all">All Listings</option>
-                        <option value="mine">Only My Listings</option>
-                        <option value="others">Other Listings</option>
-                    </select>
-                </form>
             </div>
             <br /><br />
             {searchList()}
